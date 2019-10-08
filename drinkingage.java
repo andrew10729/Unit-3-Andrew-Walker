06fa6688f164a46a79dd461bd2877bd00620adcd
Andrew <walkera@wlhs.wlwv.k12.or.us>
2019-10-08 09:52:18 -0700

    test

---
+++ drinkingage.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class drinkingage{
+  public static void main(String[]args){
+    Scanner Scan = new Scanner(System.in);
+    System.out.println("Please Enter Your Age");
+    int Scan = nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
