de12363ed1b0105d750bdd9f544b4b6686f4cd8b
Andrew <walkera@wlhs.wlwv.k12.or.us>
2019-10-22 09:46:48 -0700

    added rps

--- rps.java
+++ rps.java
@@ -3,80 +3,66 @@ import java.util.Random;
 
 public class rps{
   public static void main(String[]args){
-int count = 0;
+    //initializing gloabl variables
+    double games = 0;
+    double win = 0;
+    for(int count = 0; count>=0; count++ ) {
+      //Scanners to ask user for input
+      Scanner Scan = new Scanner(System.in);
+      System.out.println("Please Enter Your Move");
+      System.out.println("");
+      String input = Scan.nextLine();
+      int inputNumber = input.length();
+      games = games + 1;
+      String rock = "rock";
+      String paper = "paper";
 
-while(count >=0){
-  Scanner Scan = new Scanner(System.in);
-  System.out.println("Please Enter Your Move");
-  String input = Scan.nextLine();
-  int inputNumber = input.length();
-String rock = "rock";
-String paper = "paper";
+      //assigning input to the checking system
+      if(inputNumber == 4)
+        System.out.println("Your Move: Rock");
+      if(inputNumber == 5)
+        System.out.println("Your Move: Paper");
+      if(inputNumber == 8)
+        System.out.println("Your Move: Scissors");
 
+      Random rand = new Random();
+      int computerNumber = rand.nextInt(3)+1;
 
+      if(computerNumber == 1)
+        System.out.println("Computer Move: Rock");
+      if(computerNumber == 2)
+        System.out.println("Computer Move: Paper");
+      if(computerNumber == 3)
+        System.out.println("Computer Move: Scissors ");
 
-  if(inputNumber == 4)
-  System.out.println("Your Move: Rock");
-  if(inputNumber == 5)
-  System.out.println("Your Move: Paper");
-  if(inputNumber == 8)
-  System.out.println("Your Move: Scissors");
-Random rand = new Random();
-  int computerNumber = rand.nextInt(3)+1;
-  System.out.println(computerNumber);
-if(computerNumber == 1)
-System.out.println("Computer Move: Rock");
-if(computerNumber == 2)
-System.out.println("Computer Move: Papers");
-if(computerNumber == 3)
-System.out.println("Computer Move: Scissors ");
-
-if(inputNumber == 4 && computerNumber == 3){
-System.out.println("You Win");
-}
-else if(inputNumber == 4 && computerNumber == 1){
-  System.out.println("Draw");
-}else{
-System.out.println("You Lose");
-
-  System.out.println("Do you Want To Play Again? Yes or No");
-  String again = Scan.nextLine();
-  int yesNo = again.length();
-  if(yesNo == 3)
-  System.out.println("Yes");
-  count = count + 1;
-  if(yesNo == 2)
-  System.out.println("No");
-count = count-1000;
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      //comparing input move with the computer move
+      if(inputNumber == 4 && computerNumber == 3 || inputNumber == 5 && computerNumber == 1 || inputNumber 8 && computerNumber == 2){
+        System.out.println("You Win!!");
+        win = win + 1;
+      }
+      else if(inputNumber == 4 && computerNumber == 1 || inputNumber == 5 && computerNumber == 2 || inputNumber == 8 && computerNumber == 3){
+        System.out.println("Draw!");
+      }
+      else {
+        //play again sequence
+        System.out.println("You Lose :(");
+        System.out.println("Do you Want To Play Again? Yes or No");
+        String again = Scan.nextLine();
+        int yesNo = again.length();
+        if(yesNo == 3){
+          count = count + 1;
+        }
+        else{
+          //endgame sequence
+          System.out.println("No");
+          count = count-1000;
+          System.out.println("Thanks For Playing!!");
+          System.out.println("Games Played:"+ games);
+          System.out.println("Games Won: "+ win);
+          double winPercent = win/games;
+          System.out.println(winPercent*100+"%");
+        }
+      }
+    }
   }
-
 }
